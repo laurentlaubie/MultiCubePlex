7320c0e50dcf70dd92e9457ef65038342276158e
Laurent <laurent.laubie31@gmail.com>
2021-10-19 17:06:43 +0200

    backgroundimage + display image--DOING

--- MultiCubePlex/src/Controller/HomeController.php
+++ MultiCubePlex/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GamesmodesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(GamesmodesRepository $gamesmodesRepository): Response
     {
+        $gamesMode = $gamesmodesRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'homeGame' => $gamesMode,
         ]);
     }
 }

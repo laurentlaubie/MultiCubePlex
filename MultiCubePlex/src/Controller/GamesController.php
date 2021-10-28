b080fb18a99ddd37981f9ab03ff7c7a11f1ce4d8
Laurent <laurent.laubie31@gmail.com>
2021-10-28 20:39:39 +0200

    feat--new page for gamemode details

--- MultiCubePlex/src/Controller/GamesController.php
+++ MultiCubePlex/src/Controller/GamesController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Gamesmodes;
 use App\Repository\GamesmodesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+    /**
+     * @Route("/games", name="gamesmode_")
+     */
 class GamesController extends AbstractController
 {
     /**
-     * @Route("/games", name="_games")
+     * Page affichant la liste des modes de jeu
+     * 
+     * URL : /games
+     * Nom de la route : gamesmode_games
+     * 
+     * @Route("/", name="games")
      */
     public function index(GamesmodesRepository $gamesmodesRepository): Response
     {
@@ -20,4 +29,25 @@ class GamesController extends AbstractController
             'games' => $games,
         ]);
     }
+
+
+    /**
+     * Page affichant le detail d'un modes de jeu
+     * 
+     * URL : /games/{id}
+     * Nom de la route : gamesmode_show
+     * 
+     * @Route("/{id}", name="show")
+     */
+
+    public function show(Gamesmodes $gamesmodes): Response
+    {
+        return $this->render('games/show.html.twig', [
+            'game' => $gamesmodes,
+        ]);
+
+    }
+
+
+
 }
